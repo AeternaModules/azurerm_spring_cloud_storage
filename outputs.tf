eadc2f6de450f5d118477a47d1b4eb3c76be2946
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:39 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "spring_cloud_storages" {
-  description = "All spring_cloud_storage resources"
-  value       = azurerm_spring_cloud_storage.spring_cloud_storages
-}
 output "spring_cloud_storages_name" {
-  description = "List of name values across all spring_cloud_storages"
-  value       = [for k, v in azurerm_spring_cloud_storage.spring_cloud_storages : v.name]
+  description = "Map of name values across all spring_cloud_storages, keyed the same as var.spring_cloud_storages"
+  value       = { for k, v in azurerm_spring_cloud_storage.spring_cloud_storages : k => v.name }
 }
 output "spring_cloud_storages_spring_cloud_service_id" {
-  description = "List of spring_cloud_service_id values across all spring_cloud_storages"
-  value       = [for k, v in azurerm_spring_cloud_storage.spring_cloud_storages : v.spring_cloud_service_id]
+  description = "Map of spring_cloud_service_id values across all spring_cloud_storages, keyed the same as var.spring_cloud_storages"
+  value       = { for k, v in azurerm_spring_cloud_storage.spring_cloud_storages : k => v.spring_cloud_service_id }
 }
 output "spring_cloud_storages_storage_account_key" {
-  description = "List of storage_account_key values across all spring_cloud_storages"
-  value       = [for k, v in azurerm_spring_cloud_storage.spring_cloud_storages : v.storage_account_key]
+  description = "Map of storage_account_key values across all spring_cloud_storages, keyed the same as var.spring_cloud_storages"
+  value       = { for k, v in azurerm_spring_cloud_storage.spring_cloud_storages : k => v.storage_account_key }
 }
 output "spring_cloud_storages_storage_account_name" {
-  description = "List of storage_account_name values across all spring_cloud_storages"
-  value       = [for k, v in azurerm_spring_cloud_storage.spring_cloud_storages : v.storage_account_name]
+  description = "Map of storage_account_name values across all spring_cloud_storages, keyed the same as var.spring_cloud_storages"
+  value       = { for k, v in azurerm_spring_cloud_storage.spring_cloud_storages : k => v.storage_account_name }
 }
 
