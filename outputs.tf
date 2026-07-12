c1087eade786a0b543ecababb000838f4f6b4ff2
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:31:56 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "spring_cloud_storages_id" {
+  description = "Map of id values across all spring_cloud_storages, keyed the same as var.spring_cloud_storages"
+  value       = { for k, v in azurerm_spring_cloud_storage.spring_cloud_storages : k => v.id }
+}
 output "spring_cloud_storages_name" {
   description = "Map of name values across all spring_cloud_storages, keyed the same as var.spring_cloud_storages"
   value       = { for k, v in azurerm_spring_cloud_storage.spring_cloud_storages : k => v.name }
